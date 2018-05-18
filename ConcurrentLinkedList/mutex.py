ee88a194bc170e42e5c5c89de8e8c9e675523610
Clara Nobre <aclaranobre@gmail.com>
2018-05-18 14:42:29 -0300

    Criando um mutex

--- ConcurrentLinkedList/mutex.py
+++ ConcurrentLinkedList/mutex.py
@@ -1,14 +1,26 @@
 from threading import Thread, Lock
-
+import threading
 mutex = Lock()
 
-def processData(data):
-    mutex.acquire()
-    try:
-        print('Do some stuff')
-    finally:
-        mutex.release()
 
+def processData(data, thread_safe):
+    if thread_safe:
+        mutex.acquire()
+    try:
+        thread_id = threading.get_ident()
+        print('\nProcessing data:', data, "ThreadId:", thread_id)
+    finally:
+        if thread_safe:
+            mutex.release()
+
+
+counter = 0
+max_run = 100
+thread_safe = False
 while True:
-    t = Thread(target = processData, args = (some_data,))
+    some_data = counter
+    t = Thread(target=processData, args=(some_data, thread_safe))
     t.start()
+    counter = counter + 1
+    if counter >= max_run:
+        break
